d1a3d21a91909c8e9f0e5f43cf8ed63d00d46e89
Bryan Jurish <jurish@bbaw.de>
2016-03-04 11:38:03 +0000

    * updated v0.01.002 : added missing prereq DiaColloDB (oops)
    git-svn-id: file:///home/svn/dev/DiaColloDB-WWW/trunk@15842 4a515de8-beb3-11dc-84d6-2b654028afac

--- lib/DiaColloDB/WWW.pm
+++ lib/DiaColloDB/WWW.pm
@@ -13,7 +13,7 @@ use strict;
 
 ##==============================================================================
 ## Globals & Constants
-our $VERSION = "0.01.001";
+our $VERSION = "0.01.002";
 our @ISA = qw(DiaColloDB::Logger);
 
 
