5e69f1c2ac7655d80497ca440cd330385ff59f69
Bryan Jurish <jurish@bbaw.de>
2016-04-27 07:49:15 +0000

    + band-aided DiaColloDB::WWW::CGI package-global timer reset for standalone server in DiaColloDB::WWW::Handler::cgi   - should really (optionally) use object-local {t_started} in DiaColloDB::WWW::CGI
    git-svn-id: file:///home/svn/dev/DiaColloDB-WWW/trunk@16038 4a515de8-beb3-11dc-84d6-2b654028afac

--- lib/DiaColloDB/WWW/Handler/cgi.pm
+++ lib/DiaColloDB/WWW/Handler/cgi.pm
@@ -55,6 +55,7 @@ sub run {
   ##-- setup dbcgi object
   my $dbcgi =  DiaColloDB::WWW::CGI->new(%{$srv->{cgiArgs}//{}})
     or $h->logconfess("could not create DiaColloDB::WWW::CGI object: $!");
+  $dbcgi->t_start(); ##-- re-start package-global timer (hack; this should really be object-local)
   $dbcgi->{ttk_vars}{DIACOLLO_DBDIR} = $srv->{dbdir};
   $dbcgi->{ttk_vars}{dstar}{$_}      = $::dstar{$_} foreach (keys %::dstar);
   $dbcgi->fromRequest($hreq,$csock)
